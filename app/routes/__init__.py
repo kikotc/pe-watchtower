5048b00d5f0a6b3f4fe6fa540ad3c03facff96d3
Abish-27 <abishkulkarni@gmail.com>
2026-04-05 00:02:50 -0400

    Created dashboard annd login

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,8 +3,10 @@ def register_routes(app):
     from app.routes.urls import urls_bp
     from app.routes.events import events_bp
     from app.routes.observability import observability_bp
+    from app.routes.dashboard import dashboard_bp
 
     app.register_blueprint(users_bp)
     app.register_blueprint(urls_bp)
     app.register_blueprint(events_bp)
     app.register_blueprint(observability_bp)
+    app.register_blueprint(dashboard_bp)
